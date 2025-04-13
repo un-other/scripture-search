7cbfc97f8adc3750c1bf48bf379c5bed314d112f
Anthony D'Achille <adachille15@gmail.com>
2025-04-13 15:53:32 -0400

    Test simple search in main and notebook

--- scripture_search/main.py
+++ scripture_search/main.py
@@ -1,10 +1,17 @@
-"""Placeholder main file."""
+"""Run Scripture Search."""
+
+from scripture_search.search.simple_search import SimpleSearch
 
 
 def main():
-    """Placeholder."""
-    print("Hello, world!")
-
+    """Run Scripture Search."""
+    search = SimpleSearch()
+    while True:
+        query = input("Enter a query to search for scriptures or type 'q' to quit: ")
+        if query.lower() == "q":
+            break
+        results = search.run(query)
+        print(results)
 
 if __name__ == "__main__":
     main()
